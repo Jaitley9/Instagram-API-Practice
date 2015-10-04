25b17b4073fcf85d8f814695b5a74000d7dfd15d
Anmol Jaitley <jaitley9@gmail.com>
2015-10-04 16:31:09 -0400

    This was a pretty big refactoring. Changed the indexing scheme to start at zero. I made a mistake in thinking that it should've started at 1. Also changed the overall counting scheme. Now the program directly adds to the dictionary instead of relying on a list of all the users.

--- gramFan.py
+++ gramFan.py
@@ -8,59 +8,45 @@ def getLikers(userID, accessTokenID):
 		accessTokenID - Generated from authentication process. Used for access control on Instagram's end. 
 	out:
 		likers - A list of all usernames which liked every single picture in user feed
-	
-	Use the Instagram API to get a list of all the usernames that liked each post.
-	Will count the number of times a user shows up in the list for total likes by user.
 	"""
-	
-	#GET JSON data for the first 33(max) pictures for the user
-	userMediaData = requests.get("https://api.instagram.com/v1/users/" + userID +"/media/recent?access_token=" + accessTokenID + "&count=33").json()
-	likers = []
+	userMediaData = requests.get("https://api.instagram.com/v1/users/" + userID +"/media/recent?access_token=" + accessTokenID).json()
+	fans = {}
+	while(userMediaData['pagination']):
+		fans = getFans(fans, userMediaData)
+		userMediaData = requests.get(userMediaData['pagination']['next_url']).json()
 
-	while(True):
-		for i in range(1 , len(userMediaData['data'])):
-			#This request.json() function starts indexing at 1 for some reason
-			#Getting JSON data for all likes on each picture
-			likes = requests.get("https://api.instagram.com/v1/media/" + userMediaData['data'][i]['id'] + "/likes/?access_token=" + accessTokenID).json()
-			
-			#Putting usernames of every single like on all pictures into likers.
-			for j in range(1 , len(likes['data'])):
-				likers.append(likes['data'][j]['username'])
+	#Run it one last time because once you hit the last page, userMediaData['pagination'] will be false
+	return getFans(fans, userMediaData)
 
-		#Go to the next 33 posts. Instagram API makes it easy to tell when you've exhausted everything		
-		if(userMediaData['pagination']):
-			userMediaData = requests.get(userMediaData['pagination']['next_url']).json()
-		else:
-			return sorted(likers)
-
-def genFans(likers):
+def getFans(fans, userMediaData):
 	"""
 	in:
-		likers - List of usernames
-	out: 
-		fans - Dictionary of usernames and number of likes as values
-	
-	Generate a dictionary and increase count for each time they show up in the list.
+		fans - Dictionary in which the keys are the usernames of the  
 	"""
-
-	fans = {}
-
-	for userName in likers:
-		if(userName not in fans):
-			fans[userName] = 0
-		fans[userName] += 1
+	for i in range(0 , len(userMediaData['data'])):
+		likes = requests.get("https://api.instagram.com/v1/media/" + userMediaData['data'][i]['id'] + "/likes/?access_token=" + accessTokenID).json()
+		
+		for j in range(0 , len(likes['data'])):
+			userName  = likes['data'][j]['username']
+			
+			if(userName in fans):
+				fans[userName] += 1
+			else:
+				fans[userName] = 1
 
 	return fans
 
-#accessTokenID = "" You'll need to generate your own
+accessTokenID = null
+#gotta get your own accesstoken
 userID = "self"
 
-likers = getLikers(userID, accessTokenID)
-fans = genFans(likers)
+fans = getLikers(userID, accessTokenID)
 orderedFansList = sorted(fans, key = fans.get, reverse = True)
 #Thanks to this http://stackoverflow.com/a/3418035 for sorting dict via value
 
+likesList = open("userLikers.txt", 'w')
 for fan in orderedFansList:
+	likesList.write(fan + ": " + repr(fans[fan]) + '\n')
 	print(fan + ":" + repr(fans[fan]))
 	
 
