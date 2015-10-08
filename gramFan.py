c12859c4a463d4b88ca080226294673e7c30aafb
Anmol Jaitley <jaitley9@gmail.com>
2015-10-08 00:11:21 -0400

    Adding functions to get data on the posts that you've liked.
    Added two new functions and changed some function names. I'm not very
    good at this. I need to read Clean Code.

--- gramFan.py
+++ gramFan.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def getLikers(userID, accessTokenID):
+def yourFans(userID, accessTokenID):
 	"""
 	in:
 		userID - The Instagram user_id found from the API
@@ -9,22 +9,22 @@ def getLikers(userID, accessTokenID):
 	out:
 		likers - A list of all usernames which liked every single picture in user feed
 	"""
-	userMediaData = requests.get("https://api.instagram.com/v1/users/" + userID +"/media/recent?access_token=" + accessTokenID).json()
+	userMediaRecent = requests.get("https://api.instagram.com/v1/users/" + userID +"/media/recent?access_token=" + accessTokenID).json()
 	fans = {}
-	while(userMediaData['pagination']):
-		fans = getFans(fans, userMediaData)
-		userMediaData = requests.get(userMediaData['pagination']['next_url']).json()
+	while(userMediaRecent['pagination']):
+		fans = makeFansDict(fans, userMediaRecent)
+		userMediaRecent = requests.get(userMediaRecent['pagination']['next_url']).json()
 
-	#Run it one last time because once you hit the last page, userMediaData['pagination'] will be false
-	return getFans(fans, userMediaData)
+	#Run it one last time because once you hit the last page, userMediaRecent['pagination'] will be false
+	return makeFansDict(fans, userMediaRecent)
 
-def getFans(fans, userMediaData):
+def makeFansDict(fans, userMediaRecent):
 	"""
 	in:
 		fans - Dictionary in which the keys are the usernames of the  
 	"""
-	for i in range(0 , len(userMediaData['data'])):
-		likes = requests.get("https://api.instagram.com/v1/media/" + userMediaData['data'][i]['id'] + "/likes/?access_token=" + accessTokenID).json()
+	for i in range(0 , len(userMediaRecent['data'])):
+		likes = requests.get("https://api.instagram.com/v1/media/" + userMediaRecent['data'][i]['id'] + "/likes/?access_token=" + accessTokenID).json()
 		
 		for j in range(0 , len(likes['data'])):
 			userName  = likes['data'][j]['username']
@@ -36,18 +36,49 @@ def getFans(fans, userMediaData):
 
 	return fans
 
-accessTokenID = null
+def yourLikes(userID, accessTokenID):
+	userMediaLiked = requests.get("https://api.instagram.com/v1/users/" + userID +"/media/liked?access_token=" + accessTokenID).json()
+	likes = {}
+	
+	while(userMediaLiked['pagination']):
+		
+		likes = makeLikesDict(likes, userMediaLiked)
+		userMediaLiked = requests.get(userMediaLiked['pagination']['next_url']).json()
+	
+	#Run it one last time because once you hit the last page, userMediaLiked['pagination'] will be false
+	return makeLikesDict(likes, userMediaLiked)
+
+def makeLikesDict(likes, userMediaLiked):
+	for i in range(0 , len(userMediaLiked['data'])):
+		
+		userName  = userMediaLiked['data'][i]['user']['username']
+		
+		if(userName in likes):
+			likes[userName] += 1
+		else:
+			likes[userName] = 1
+
+	return likes
+
+
+
+#accessTokenID = null
 #gotta get your own accesstoken
+accessTokenID = "835826709.be662d5.2208942e2bce465a80eda436ca9e60a4"
+#https://api.instagram.com/v1/users/self/media/liked?access_token=835826709.be662d5.2208942e2bce465a80eda436ca9e60a4
+
 userID = "self"
 
-fans = getLikers(userID, accessTokenID)
-orderedFansList = sorted(fans, key = fans.get, reverse = True)
+#fans = yourFans(userID, accessTokenID)
+liked = yourLikes(userID, accessTokenID)
+#orderedFansList = sorted(fans, key = fans.get, reverse = True)
+orderedLikedList  = sorted(liked, key = liked.get, reverse = True)
 #Thanks to this http://stackoverflow.com/a/3418035 for sorting dict via value
 
 likesList = open("userLikers.txt", 'w')
-for fan in orderedFansList:
-	likesList.write(fan + ": " + repr(fans[fan]) + '\n')
-	print(fan + ":" + repr(fans[fan]))
+for idol in orderedLikedList:
+	likesList.write(idol + ": " + repr(liked[idol]) + '\n')
+	print(idol + ":" + repr(liked[idol]))
 	
 
 	
